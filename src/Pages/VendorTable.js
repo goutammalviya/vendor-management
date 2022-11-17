b3a4428a8fa2e25767b825925a160e0552079cd1
kuldeep <kuldeeppanwar7697@gmail.com>
2022-11-17 14:00:12 +0530

    delete

--- src/Pages/VendorTable.js
+++ src/Pages/VendorTable.js
@@ -4,35 +4,100 @@ import { VendorsColumns } from "../Components/Table/TableColumns";
 import { useNavigate } from "react-router-dom";
 import sheetService, { getSheetRows } from "../Services/SheetService2";
 const VendorTable = () => {
-  const columns = useMemo(() => VendorsColumns);
+  
   const [loading , setLoading] = useState(false)
   let sheet = null;
   const [vendors, setVendors] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
-    setLoading(true)
-    const asyncFn = async () => {
-      const sheets = await sheetService("vendors");
-      sheet = sheets;
-      let data = await getSheetRows(sheet);
-      setVendors(data);
-      setLoading(false)
-    };
-    asyncFn();
+    reFetchData(true)
   }, []);
 
-  useEffect(() => {
-    setLoading(true)
-    const asyncFn = async () => {
-      const sheets = await sheetService("vendors");
-      sheet = sheets;
-      let data = await getSheetRows(sheet);
-      setVendors(data);
-      setLoading(false)
+ 
+  const reFetchData = async (showLoader) => {
+    showLoader && setLoading(true);
+    const sheets = await sheetService("vendors");
+    sheet = sheets;
+    let data = await getSheetRows(sheet);
+    setVendors(data);
+    showLoader && setLoading(false);   
     };
-    asyncFn();
-  }, [columns , ]);
-  
+
+const deleteRow = async row =>{
+
+  await row.delete();
+  await reFetchData(false)
+
+
+}
+  const columns = useMemo(() =>  [
+    {
+        Header: 'address',
+        accessor: 'address'
+    },
+    {
+        Header: 'company name',
+        accessor: 'company name'
+    },
+    {
+        Header: 'contact no',
+        accessor: 'contact no'
+    },
+    {
+        Header: 'contact person name',
+        accessor: 'contact person name'
+    },
+    {
+        Header: 'email',
+        accessor: 'email'
+    },
+    {
+        Header: 'email address',
+        accessor: 'email address'
+    },
+    {
+        Header: 'linkedIn url',
+        accessor: 'linkedIn url'
+    },
+    {
+        Header: 'location',
+        accessor: 'location'
+    },
+    {
+        Header: 'profile name',
+        accessor: 'profile name'
+    },
+    {
+        Header: 'vendor list category',
+        accessor: 'vendor list category'
+    },
+    {
+        Header: 'website url',
+        accessor: 'website url'
+    },
+    {
+        Header: 'CRUD',
+        accessor: 'CRUD',
+        Cell: ({ row: { original } }) => {
+            console.log(original);
+			return (
+				<>
+					<span className='' onClick={()=>deleteRow(original)}>
+						delete
+					</span>
+					<span className='px-1'>
+						000
+					</span>
+					<span className=''>
+						000
+					</span>
+				</>
+			)
+		}
+    },
+]
+
+);
   console.log(vendors);
   if(loading){
     return (
