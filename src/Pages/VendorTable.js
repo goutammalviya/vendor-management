fdb03938890dbab051b0f675b347b1e67cc53ccb
goutam <goutam.vishwkarma@baelworks.com>
2022-11-17 12:47:47 +0530

    a

--- src/Pages/VendorTable.js
+++ src/Pages/VendorTable.js
@@ -5,19 +5,27 @@ import { useNavigate } from "react-router-dom";
 import sheetService, { getSheetRows } from "../Services/SheetService2";
 const VendorTable = () => {
   const columns = useMemo(() => VendorsColumns);
+  const [loading , setLoading] = useState(false)
   let sheet = null;
   const [vendors, setVendors] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
+    setLoading(true)
     const asyncFn = async () => {
       const sheets = await sheetService("vendors");
       sheet = sheets;
       let data = await getSheetRows(sheet);
       setVendors(data);
+      setLoading(false)
     };
     asyncFn();
   }, []);
   console.log(vendors);
+  if(loading){
+    return (
+      <><h1>Loading....</h1></>
+    )
+  }
   return (
     <div>
       <BasicTable
